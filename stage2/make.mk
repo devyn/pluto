b69e337a3d9ee222586d46674a094946dbcef955
Devyn Cairns <devyn.cairns@gmail.com>
2023-08-16 18:46:01 -0700

    actually fix shutdown in makefile lol

--- stage2/make.mk
+++ stage2/make.mk
@@ -1,9 +1,7 @@
 stage2.lsp: $(wildcard stage2/*.lsp)
 	# strip comments
 	sed -E '/^ *(;.*)?$$/d' $^ > stage2.lsp
-	if [[ "${SHUTDOWN}" == 1 ]]; then \
-		echo "(call-native shutdown\\$$ 0)" >> stage2.lsp \
-	fi
+	[[ "${SHUTDOWN}" == 1 ]] && echo "(call-native shutdown$$ 0)" >> stage2.lsp || true
 
 clean-stage2:
 	rm -f stage2.lsp
