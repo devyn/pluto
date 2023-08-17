afa5e7f369140d4277ce912ccf1a2af49759f4c7
Devyn Cairns <devyn.cairns@gmail.com>
2023-08-16 18:25:44 -0700

    fix escape of dollar sign in shutdown in stage2 makefile

--- stage2/make.mk
+++ stage2/make.mk
@@ -2,7 +2,7 @@ stage2.lsp: $(wildcard stage2/*.lsp)
 	# strip comments
 	sed -E '/^ *(;.*)?$$/d' $^ > stage2.lsp
 	if [[ "${SHUTDOWN}" == 1 ]]; then \
-		echo "(call-native shutdown$$ 0)" >> stage2.lsp \
+		echo "(call-native shutdown\\$$ 0)" >> stage2.lsp \
 	fi
 
 clean-stage2:
